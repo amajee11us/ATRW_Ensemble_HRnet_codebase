300329e318139da6c51b6d41e09b849ce6d1c6d6
Anay Majee <anaymajee11us@gmail.com>
2019-08-06 00:44:46 +0530

    Identify tiger as a dataset

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -8,5 +8,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .mpii import MPIIDataset as mpii
-from .coco import COCODataset as coco
+#from .mpii import MPIIDataset as mpii
+from .tiger import COCODataset as tiger
