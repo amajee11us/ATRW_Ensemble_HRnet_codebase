c53ed60c37617897787e60ccd054d270e8530954
amajee11us <52267538+amajee11us@users.noreply.github.com>
2019-08-05 20:51:57 +0530

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2018-present Microsoft
 # Licensed under The Apache-2.0 License [see LICENSE for details]
 # Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# Edited by Anay Majee (anay.majee@intel.com)
 # ------------------------------------------------------------------------------
 
 from __future__ import absolute_import
